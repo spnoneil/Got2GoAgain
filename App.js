77a93ca3c0a79ad2a4a9a7deaa0fcce232b9f554
Scott O'Neil <scottieoneil@gmail.com>
2021-05-18 17:05:18 -0700

    STRUGGLE

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen'
 import DetailsScreen from './src/screens/DetailsScreen'
+
 const Stack = createStackNavigator();
 
 function App() {
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,83 @@
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import React from 'react';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import React, {Component, useState} from 'react';
+import useLocation from './Location'
 
+// function Map() {
+// //   const location = useLocation();
+// // console.log(location);
+//   return(
+//     <MapView
+//       style={{height: `50%`, width: `100%`}}
+//       provider={PROVIDER_GOOGLE}
+//       showsUserLocation={true}
+//       // initialRegion={{
+//       // latitude: 45.50527180254351,
+//       // longitude: -122.67506901733977,
+//       // latitudeDelta: 0.0922,
+//       // longitudeDelta: 0.0421}}
+//     >
+//       {/* <Marker draggable
+//       // coordinate={initialRegion}
+//       image={require('./../assets/marker.png')}
+//       // onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
+//       /> */}
+//       <Marker coordinate={{ latitude: 51.5078788, longitude: -0.0877321 }} />
+//       </MapView>
+//   );
+// }
 
+// export default Map;
 
-function Map() {
-  return(
-    <MapView
-      style={{height: `50%`, width: `100%`}}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation={true}
-      initialRegion={{
-      latitude: 45.50527180254351,
-      longitude: -122.67506901733977,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421}}
-    />
-  );
+class Map extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { region : {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+  
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return(
+      <MapView
+        style={{height: `50%`, width: `100%`}}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        initialRegion={{
+        latitude: 45.50527180254351,
+        longitude: -122.67506901733977,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421}}
+        onRegionChange={region => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
+        }}
+        onRegionChangeComplete={region => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
+        }}
+      >
+        {/* <Marker draggable
+        coordinate={{latitude: 45.50527180254351, longitude: -122.67506901733977 }}
+        image={require('./../assets/marker.png')}
+        // onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
+        /> */}
+        <Marker coordinate={{ latitude: 51.5078788, longitude: -0.0877321 }} />
+        </MapView>
+    );
+  }
 }
 
 export default Map;
