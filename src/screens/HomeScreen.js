a196003a5c1da59354e24e33f18495dae0770dc8
Scott O'Neil <scottieoneil@gmail.com>
2021-05-19 11:12:54 -0700

    add logo to home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,18 +2,32 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import Map from './../components/Map'
+import { Image, StyleSheet } from 'react-native'
 
 function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home stuff</Text>
+      <Image source={require('./../assets/logo.png')} style={styling.image} />
+      {/* <Text>Home stuff</Text> */}
       <Map />
       <Button
+      style={styling.button}
         title="Add a Bathroom"
         onPress={() => navigation.navigate('AddBathroom')}
       />
     </View>
   );
 }
+
+const styling = StyleSheet.create({
+  image: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  button: {
+    paddingTop: 3
+  }
+})
 
 export default HomeScreen;
